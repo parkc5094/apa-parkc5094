c122ddf0428a3f19f194d4a902ab030b0892d815
parkc5094 <47994407+parkc5094@users.noreply.github.com>
2019-04-16 20:47:41 -0700

    test

--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -7,5 +7,9 @@ public class OddToEvenRunner
 {
   public static void main( String args[] )
   {
+	ArrayList <Integer> ray = new ArrayList<Integer> 
+(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11);
+	
+	go();
   }
 }
